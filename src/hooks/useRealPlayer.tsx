827f524c623ecd59d22f1fc8fe5235ea23a1aea7
Anna Kryva <anna120100@gmail.com>
2020-07-16 11:13:31 +0300

    Adjusted real user hook

--- src/hooks/useRealPlayer.tsx
+++ src/hooks/useRealPlayer.tsx
@@ -1,29 +1,16 @@
 import React from 'react';
-import delay from 'delay';
 import { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { MatchButton } from '../components/MatchButton';
 import { PlayerConfiguration } from '../types';
-import { COLORS } from '../theme';
 
 const useRealPlayer = (player: PlayerConfiguration) => {
   const { leftMatches, isTurn, makeMove } = player;
   const [isDisabled, setIsDisabled] = useState(isTurn);
-  const [isDelayed, setIsDelayed] = useState(false);
-  let selectedNumber = 0;
-
+  
   useEffect(() => {
-    setIsDelayed(false);
     setIsDisabled(!isTurn);
   }, [isTurn]);
-
-  const makeMoveHolder = async (numberOfMatches: number) => {
-    selectedNumber = numberOfMatches;
-    setIsDisabled(true);
-    setIsDelayed(true);
-    await delay(1000);
-    makeMove(numberOfMatches);
-  };
 
   const buttons = [1, 2, 3].map((value) => {
     if (value <= leftMatches) {
@@ -31,7 +18,7 @@ const useRealPlayer = (player: PlayerConfiguration) => {
         <MatchButton
           key={value}
           numberOfMatches={value}
-          onSubmit={makeMoveHolder}
+          onSubmit={makeMove}
           disabled={isDisabled}
         />
       );
@@ -41,9 +28,6 @@ const useRealPlayer = (player: PlayerConfiguration) => {
   return (
     <View style={styles.container}>
       <View style={styles.buttonsBlock}>{buttons}</View>
-      {/* <Text style={styles.status}>
-        {isDelayed ? `Player took ${selectedNumber} matches` : ''}
-      </Text> */}
     </View>
   );
 };
@@ -57,10 +41,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'nowrap',
     justifyContent: 'space-around',
-  },
-  status: {
-    fontSize: 20,
-    color: COLORS.WHITE,
   },
 });
 
